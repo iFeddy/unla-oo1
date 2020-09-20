26e0d0d1b43bfc8f8d8ece8adc9c360083cfcfa4
Feddy <feddy_7990@hotmail.com>
2020-09-20 14:32:08 -0300

    Movido GitIgnore a Raiz

--- TPI/src/test/TestAlmacen.java
+++ TPI/src/test/TestAlmacen.java
@@ -7,7 +7,7 @@ public class TestAlmacen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Articulo art1 = new Articulo(1, "nada", "123456787678", 12);
-		System.out.println(art1.isValidBarCodeEAN("0054321223457"));
+		System.out.println(art1.isValidBarCodeEAN("0054321223457s"));
 	}
 
 }
